a99be4054dad169c621b9f6dc4624cd3e2f31448
James Robertson <james@r0bertson.co.uk>
2022-02-01 20:57:14 +0000

    minor bug fix: A value can appear directly after the colon.

--- lib/rowx.rb
+++ lib/rowx.rb
@@ -8,15 +8,15 @@ require 'line-tree'
 class RowXException < Exception
 end
 
-# for an example of ArrayCollate 
+# for an example of ArrayCollate
 # see http://www.jamesrobertson.eu/snippets/2014/jun/08/collating-items-in-an-array.html
 
 module ArrayCollate
   refine Array do
     def collate(pattern=nil)
       a = self.inject([[]]) do |r,x|
-        if block_given? then  
-          yield(x) ? r << [x] : r[-1] << x        
+        if block_given? then
+          yield(x) ? r << [x] : r[-1] << x
         else
           x =~ pattern ? r << [x] : r[-1] << x
         end # block
@@ -24,25 +24,25 @@ module ArrayCollate
       end
       a.shift if a.first.empty?
       a
-    end 
+    end
   end
 end
 
 class RowX
-  
+
   using ArrayCollate
-  using ColouredText  
+  using ColouredText
 
   attr_reader :to_a, :to_xml, :to_lines
 
-  def initialize(txt, level: nil, ignore_blank_lines: false, 
-                 abort_1_row: false, debug: false, name: 'item', 
+  def initialize(txt, level: nil, ignore_blank_lines: false,
+                 abort_1_row: false, debug: false, name: 'item',
                  allow_lonely_keyfield: false)
-     
+
     @name, @debug = name, debug
-    
-    # auto indent any multiline values   
-    
+
+    # auto indent any multiline values
+
     indent = ''
 
     lines = txt.gsub(/^-+$/m,'').lines.map do |line|
@@ -55,10 +55,10 @@ class RowX
       end
 
     end
-    
+
     puts 'lines: ' + lines.inspect if @debug
 
-    a = LineTree.new(lines.join, level: level, 
+    a = LineTree.new(lines.join, level: level,
                      ignore_blank_lines: ignore_blank_lines, debug: debug).to_a
     puts ('a: ' + a.inspect).debug if @debug
 
@@ -66,23 +66,23 @@ class RowX
     puts ('keyfield: ' + keyfield.inspect).debug if @debug
 
     if not allow_lonely_keyfield then
-      
-      # find the keyfield. if there's only 1 keyfield in all of the rows it's 
+
+      # find the keyfield. if there's only 1 keyfield in all of the rows it's
       # not a keyfield. Keep searching until all rows have been searched
       while a.select {|x| x[0][/^#{keyfield}/]}.length <= 1 and \
                                                         i < a.length and a[i+1]
-        
+
         i += 1
         keyfield = a[i][0][/\w+/]
 
       end
-      
+
     end
-    
+
     keyfield = a[0][0][/\w+/] if i == a.length - 1
 
     if a.flatten(1).grep(/^#{keyfield}/).length == 1 then # only 1 record
-      i = 0 
+      i = 0
       raise RowXException, 'Expected more than 1 row' if abort_1_row
     end
 
@@ -90,43 +90,43 @@ class RowX
 
     summary = scan_a a.slice!(0,i)
     summary[0] = 'summary'
-    
+
     @rexle_a = scan_records(records, level)
     @to_a = ['root', {}] + [summary] + @rexle_a
     @to_xml = Rexle.new(@to_a).xml pretty: true
 
   end
-  
+
   def to_lines(delimiter: ' # ')
     @rexle_a.map {|x| x[3..-1].map {|y| y[2]}.join(delimiter)}.join("\n").lines
   end
 
   private
-  
+
 
   def scan_a(row)
 
     a = row.map do |field|
-      
+
       puts 'field: ' + field.inspect if @debug
       s = field.is_a?(Array) ? field[0] : field
 
       return if s.empty?
       puts 's: ' + s.inspect if @debug
-      found = s.match(/^(\w+)(?:\:$|\:\s+)(.*)/m)      
+      found = s.match(/^(\w+)(?:\:$|\:\s*)(.*)/m)
 
-      value, name = found ? found.captures.reverse : s      
+      value, name = found ? found.captures.reverse : s
       name ||= 'description'
-      
-      children = scan_a(field[1..-1]) if field[-1] .is_a?(Array) 
-      value = value.to_s.strip.gsub('<','&lt;').gsub('>','&gt;')      
+
+      children = scan_a(field[1..-1]) if field[-1] .is_a?(Array)
+      value = value.to_s.strip.gsub('<','&lt;').gsub('>','&gt;')
 
       # it's a line which has been commented out?
       if name[0] == '#' then
         value = name[1..-1] + ': ' + value
-        name = '!-' 
+        name = '!-'
       end
-        
+
       result = [name, {}, value]
       result << children if children
       result
@@ -136,7 +136,7 @@ class RowX
   end
 
   def scan_records(rows, level)
-    
+
     rows.map {|x| scan_a x }
 
   end
--- rowx.gemspec
+++ rowx.gemspec
@@ -1,16 +1,16 @@
 Gem::Specification.new do |s|
   s.name = 'rowx'
-  s.version = '0.7.4'
+  s.version = '0.8.0'
   s.summary = 'rowx'
   s.description = 'Generates XML from rows of labelled text, nested text, ' + 
       ' and plain text' 
   s.authors = ['James Robertson']
   s.files = Dir['lib/rowx.rb']
-  s.add_runtime_dependency('line-tree', '~> 0.9', '>=0.9.1')
+  s.add_runtime_dependency('line-tree', '~> 0.9', '>=0.9.3')
   s.signing_key = '../privatekeys/rowx.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/rowx'
   s.required_ruby_version = '>= 2.1.2'
 end
